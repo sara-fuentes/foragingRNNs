81928c0710588ece5d03147600fa50e42a3aa3e6
sara-fuentes <sarafuentessimon@gmail.com>
2024-05-23 00:33:42 +0200

    fix errors, solve issues, comment cumulative trial rate plot

--- mice_analyses.py
+++ mice_analyses.py
@@ -116,6 +116,14 @@ def get_regressors(df, num_trial_back=10, iti=None):
 
     return df_glm, regressors
 
+# Define a custom converter function to clean and convert iti_duration values
+def clean_iti_duration(value):
+    try:
+        # Remove periods and convert to float
+        return float(value.replace('.', ''))
+    except ValueError:
+        # Handle the case where conversion fails
+        return None
 
 
 if __name__ == '__main__':
@@ -125,9 +133,20 @@ if __name__ == '__main__':
     # 1.1. Generalize code for mice using the code for RNNs
     # 2. Compute GLM conditioning on mouse (there are 3 mice in the dataset)
     # 3. Introduce ITI as a regressor
-    data_folder= '/home/molano/Dropbox/Molabo/foragingRNNs/mice/'
-    filename ='global_trials_100524.csv'
-    df = pd.read_csv(str(data_folder) + str(filename), sep=';', low_memory=False)
+    data_folder = 'C:\\Users\\saraf\\OneDrive\\Documentos\\IDIBAPS\\foraging RNNs\\mice\\'
+    # data_folder= '/home/molano/Dropbox/Molabo/foragingRNNs/mice/'
+    filename ='global_trials_sara.csv'
+    # Read the CSV file and apply the custom converter function to the iti_duration column
+    # try:
+    #     df = pd.read_csv(str(data_folder) + str(filename), 
+    #                      sep=';', 
+    #                      converters={'iti_duration': clean_iti_duration},
+    #                      low_memory=False)
+    # except Exception as e:
+    #     print(f"An error occurred: {e}")
+
+# Read the CSV file and specify that a column (e.g., 'column_name') should be read as a float
+    df = pd.read_csv(str(data_folder) + str(filename), sep=';', low_memory=False, dtype={'iti_duration': float})
     # get only trials with iti
     df = df[df['task'] != 'S4'] # quita las sessiones sin ITI
     df = df[df['subject'] != 'manual'] #
@@ -138,8 +157,8 @@ if __name__ == '__main__':
         df_mice['iti_bins'], bins_iti = pd.qcut(df_mice['iti_duration'], num_bins_iti, labels=False, retbins=True)
         for iti_index in range(num_bins_iti):
             iti = [bins_iti[iti_index], bins_iti[iti_index + 1]]
-            df_mice, regressors = get_regressors(df_mice, iti=iti)
-            mM_logit = smf.logit(formula='choice_num ~ ' + regressors, data=df_mice).fit()
+            df_glm_mice, regressors = get_regressors(df=df_mice, iti=iti)
+            mM_logit = smf.logit(formula='choice_num ~ ' + regressors, data=df_glm_mice).fit()
             GLM_df = pd.DataFrame({
                 'coefficient': mM_logit.params,
                 'std_err': mM_logit.bse,
@@ -155,7 +174,7 @@ if __name__ == '__main__':
     # you can add multiple regressors by making them interact: "+" for only fitting separately,
     # "*" for also fitting the interaction
     # Apply glm
-    mM_logit = smf.logit(formula='choice_num ~ ' + regressors, data=df).fit()
+    # mM_logit = smf.logit(formula='choice_num ~ ' + regressors, data=GLM_df).fit()
  
     # prints the fitted GLM parameters (coefs), p-values and some other stuff
     results = mM_logit.summary()
@@ -186,40 +205,44 @@ if __name__ == '__main__':
     # axes.set_ylabel('GLM weight', label_kwargs)
     # axes.set_xlabel('Prevous trials', label_kwargs)
     plt.legend()
+    plt.show()
 
 
     #### last PLOT : CUMULATIVE TRIAL RATE
-    axes = plt.subplot2grid((50, 50), (39, 36), rowspan=11, colspan=14)
+    # axes = plt.subplot2grid((50, 50), (39, 36), rowspan=11, colspan=14)
 
-    df['start_session'] = df.groupby(['subject', 'session'])['STATE_center_light_START'].transform('min')
-    df['end_session'] = df.groupby(['subject', 'session'])['STATE_drink_delay_END'].transform('max')
-    df['session_lenght'] = (df['end_session'] - df['start_session']) / 60
-    df['current_time'] = df.groupby(['subject', 'session'])['STATE_center_light_START'].transform(
-        lambda x: (x - x.iloc[0]) / 60)  # MINS
+    # df['start_session'] = df.groupby(['subject', 'session'])['STATE_center_light_START'].transform('min')
+    # # Remove commas if necessary and convert to float
+    # df['start_session'] = df['start_session'].str.replace(',', '').astype(float)
+    # df['end_session'] = df.groupby(['subject', 'session'])['STATE_drink_delay_END'].transform('max')
+    # df['session_lenght'] = (df['end_session'] - df['start_session']) / 60
+    # df['STATE_center_light_START'] = df['STATE_center_light_START'].str.replace(',', '').astype(float)    
+    # df['current_time'] = df.groupby(['subject', 'session'])['STATE_center_light_START'].transform(
+    #     lambda x: (x - x.iloc[0]) / 60)  # MINS
 
-    max_timing = round(df['session_lenght'].max())
-    max_timing = int(max_timing)
-    sess_palette = sns.color_palette('Greens', 5)  # color per day
+    # max_timing = round(df['session_lenght'].max())
+    # max_timing = int(max_timing)
+    # sess_palette = sns.color_palette('Greens', 5)  # color per day
 
-    for idx, day in enumerate(df.day.unique()):
-        subset = df.loc[df['day'] == day]
-        n_sess = len(subset.session.unique())
-        try:
-            hist_ = stats.cumfreq(subset.current_time, numbins=max_timing,
-                                    defaultreallimits=(0, subset.current_time.max()), weights=None)
-        except:
-            hist_ = stats.cumfreq(subset.current_time, numbins=max_timing, defaultreallimits=(0, max_timing),
-                                    weights=None)
-        hist_norm = hist_.cumcount / n_sess
-        bins_plt = hist_.lowerlimit + np.linspace(0, hist_.binsize * hist_.cumcount.size, hist_.cumcount.size)
-        sns.lineplot(x=bins_plt, y=hist_norm, color = sess_palette[idx % len(sess_palette)], ax=axes, marker='o', markersize=4)
+    # for idx, day in enumerate(df_mice['day'].unique()):
+    #     subset = df_mice.loc[df_mice['day'] == day]
+    #     n_sess = len(subset['session'].unique())
+    #     try:
+    #         hist_ = stats.cumfreq(subset['current_time'], numbins=max_timing,
+    #                                 defaultreallimits=(0, subset['current_time'].max()), weights=None)
+    #     except Exception as e:
+    #         hist_ = stats.cumfreq(subset['current_time'], numbins=max_timing, defaultreallimits=(0, max_timing),
+    #                                 weights=None)
+    #     hist_norm = hist_.cumcount / n_sess
+    #     bins_plt = hist_.lowerlimit + np.linspace(0, hist_.binsize * hist_.cumcount.size, hist_.cumcount.size)
+    #     sns.lineplot(x=bins_plt, y=hist_norm, color = sess_palette[idx % len(sess_palette)], ax=axes, marker='o', markersize=4)
 
-    # axes.set_ylabel('Cum. nº of trials', label_kwargs)
-    # axes.set_xlabel('Time (mins)', label_kwargs)
+    # # axes.set_ylabel('Cum. nº of trials', label_kwargs)
+    # # axes.set_xlabel('Time (mins)', label_kwargs)
 
-    # legend
-    lines = [Line2D([0], [0], color=sess_palette[i], marker='o', markersize=7, markerfacecolor=sess_palette[i]) for
-                i in
-                range(len(sess_palette))]
-    axes.legend(lines, np.arange(-5, 0, 1), title='Days', loc='center', bbox_to_anchor=(0.1, 0.85))
-    plt.show()
+    # # legend
+    # lines = [Line2D([0], [0], color=sess_palette[i], marker='o', markersize=7, markerfacecolor=sess_palette[i]) for
+    #             i in
+    #             range(len(sess_palette))]
+    # axes.legend(lines, np.arange(-5, 0, 1), title='Days', loc='center', bbox_to_anchor=(0.1, 0.85))
+    # plt.show()
